ea7915c8431ad7c0922fdda6c3985fbbdaec76df
Ming Lin <linmi@umich.edu>
2016-10-26 13:26:20 -0400

    finish gFM BatchSolver, ready to release

--- tutorial/demo_gFM_BatchSolver_low_level_interface.py
+++ tutorial/demo_gFM_BatchSolver_low_level_interface.py
@@ -27,28 +27,25 @@ X_normalized = (X-X.mean(axis=1,keepdims=True))/X.std(axis=1,keepdims=True)
 #---------- Synthetic $M^*$ and $w^*$ as our ground-truth gFM model
 
 U_true = numpy.random.randn(d,k)
+U_true_unit,_ = numpy.linalg.qr(U_true)
 M_true = numpy.dot(U_true,U_true.T)
-M_true /= numpy.linalg.norm(M_true)
-U,s,V = numpy.linalg.svd(M_true)
-V = s[:,numpy.newaxis] * V
-U_true = U
-V_true = V.T
+M_true = M_true/numpy.linalg.norm(M_true)
 w_true = numpy.random.randn(d,1)
-w_true /= numpy.linalg.norm(w_true)
+w_true = w_true/numpy.linalg.norm(w_true)
 
-y = X_normalized.T.dot(w_true) +  gFM.A_(X_normalized,U_true,V_true) # synthetize true labels
+y = X_normalized.T.dot(w_true) +  gFM.A_(X_normalized,U_true,U_true) # generate true labels for training
 
 #---------- Initialize gFM ----------#
 print 'Initializing gFM minibatch ...'
 
-# Create a new gFM model. The model is of rank k. We choose the regularizer lambd_M and lambd_w to be the twice of the norm of ground-truth.
+# Create a new gFM solver. The solver is of rank rank_k. We choose the regularizer lambda_M and lambda_w to be the twice of the norm of ground-truth. The learning rate by default is 1.
 # Only rank_k must be specified here.
 my_gFM_solver = gFM.BatchSolver(rank_k=k,
-                                    lambd_M=numpy.linalg.norm(U_true) ** 2 * 2,
-                                    lambd_w=numpy.linalg.norm(w_true) * 2, )
+                                lambda_M=numpy.linalg.norm(U_true) ** 2 * 2,
+                                lambda_w=numpy.linalg.norm(w_true) * 2, learning_rate = 1)
 
-# Initialization stage of the gFM. Iterate 10 loops in the initialization stage.
-my_gFM_solver.initialization(X,y,max_init_iter=200)
+# Initialization stage of the gFM. Iterate 20 loops in the initialization stage.
+my_gFM_solver.initialization(X.T,y.flatten(),max_init_iter=20)
 
 T = 200 # The number of iterations in the training stage
 err_record = numpy.zeros((20,)) # record the estimation error along iteration
@@ -57,7 +54,7 @@ err_record_count = 0
 for t in xrange(T):
     # invoke one training iteration. For maximum efficient, it is better to first z-score normalized X along feature dimension then pass the parameter
     # z_score_normalized = True when calling iterate_train()
-    my_gFM_solver.iterate_train(X, y,max_iter=1)
+    my_gFM_solver.iterate_train(X.T, y.flatten(),max_iter=1)
 
     # record the estimation error at step t
     if t == error_record_t[err_record_count]:
