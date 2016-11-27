a9677e8cb97611c4073ff1abfc919d266423cdad
Ming Lin <linmi@umich.edu>
2016-11-27 14:26:43 -0500

    add: gFM with diag zero

--- gFM.py
+++ gFM.py
@@ -22,7 +22,7 @@ class BatchRegression(BaseEstimator, RegressorMixin):
     """
     def __init__(self, rank_k=None, max_iter=None, tol=1e-6, max_init_iter=None, init_tol=None,
                  lambda_M=numpy.Inf, lambda_w=numpy.Inf, learning_rate=1.0,solver_algorithm='state-of-the-art', using_cache=True,
-                 learn_bias_term=True,learn_w=True):
+                 learn_bias_term=True,learn_w=True, diag_zero=True):
         """
         Initialize a gFM_BatchSolver instance.
         :param rank_k: The rank of the target second order matrix in gFM ($M^*$). Should be of type int.
@@ -49,9 +49,228 @@ class BatchRegression(BaseEstimator, RegressorMixin):
         self.using_cache = using_cache
         self.learn_bias_term = learn_bias_term
         self.learn_w = learn_w
+        self.diag_zero = diag_zero
         return
 
-    def fit(self,X,y=None, sample_weight=None, n_more_iter=None, X_is_zscore_normalized = False):
+    def fit_diag_not_zero(self,X,y=None, sample_weight=None, n_more_iter=None, X_is_zscore_normalized = False):
+        # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray) -> object
+        """
+        Train gFM with data X and label y.
+        :param X: Feature matrix, $n \times d$.
+        :param y: Label vector, shape=(n,)
+        :param sample_weight: optional, weight for each samples, shape=(n,)
+        :param n_more_iter: optional, number of iterations in this fit() call
+        :param X_is_zscore_normalized: optional, if X has been z-score normalized, that is, each feature is zero-mean and unit variance, set this to True to save the computation for normalization in the fit() function.
+        :return: self
+        """
+        if self.diag_zero:
+            return self.fit_diag_zero(X,y, sample_weight, n_more_iter, X_is_zscore_normalized)
+        else:
+            return self.fit_diag_not_zero(X, y, sample_weight, n_more_iter, X_is_zscore_normalized)
+    pass # end def
+
+    def fit_diag_zero(self,X,y=None, sample_weight=None, n_more_iter=None, X_is_zscore_normalized = False):
+        # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray) -> object
+        """
+        Train gFM with data X and label y.
+        :param X: Feature matrix, $n \times d$.
+        :param y: Label vector, shape=(n,)
+        :param sample_weight: optional, weight for each samples, shape=(n,)
+        :param n_more_iter: optional, number of iterations in this fit() call
+        :param X_is_zscore_normalized: optional, if X has been z-score normalized, that is, each feature is zero-mean and unit variance, set this to True to save the computation for normalization in the fit() function.
+        :return: self
+        """
+        X = X.T
+        y = y[:, numpy.newaxis]
+        y = numpy.asarray(y, dtype=numpy.float)
+        n = X.shape[1]
+
+
+        if sample_weight is None:
+            sample_weight = numpy.ones((n,))
+            sample_weight = sample_weight/numpy.sum(sample_weight)
+        sample_weight = sample_weight[:, numpy.newaxis]
+
+        X_new = None
+
+        if not hasattr(self,'is_init_stage_'):
+            self.is_init_stage_ = True
+            self.remaining_init_iter_steps_ = self.max_init_iter
+            self.remaining_train_iter_steps_ = self.max_iter
+            self.d = X.shape[0]
+            X_times_sample_weight = n * X * sample_weight.T
+
+            if not X_is_zscore_normalized:
+                self.data_mean = X_times_sample_weight.mean(axis=1, keepdims=True)
+                X_new = X - self.data_mean
+            else:
+                self.data_mean = numpy.zeros((X.shape[0],1))
+                X_new = X
+            X_weighted_std = numpy.sqrt(n * numpy.mean((X_new ** 2) * sample_weight.T, axis=1, keepdims=True))
+
+            if not X_is_zscore_normalized:
+                self.data_std = numpy.maximum(X_weighted_std, 1e-12)
+                X_new /= self.data_std
+            else:
+                self.data_std = numpy.ones((X.shape[0],1))
+
+            if self.using_cache:
+                self.cached_Xp2_ = X_new ** 2
+                self.cached_Xp3_ = self.cached_Xp2_ * X_new
+                self.cached_Xp4_ = self.cached_Xp3_ * X_new
+            pass # end if self.using_cache
+
+            print 'gFM with diag-zero'
+
+            if self.using_cache:
+                self.data_moment3 = numpy.mean(n * (self.cached_Xp3_) * sample_weight.T, axis=1, keepdims=True)
+                self.data_moment4 = numpy.mean(n * (self.cached_Xp4_) * sample_weight.T, axis=1, keepdims=True)
+            else:
+                self.data_moment3 = numpy.mean(n * (X_new**3) * sample_weight.T, axis=1, keepdims=True)
+                self.data_moment4 = numpy.mean(n * (X_new**4) * sample_weight.T, axis=1, keepdims=True)
+            pass # end if self.using_cache
+
+            U, _ = numpy.linalg.qr(numpy.random.randn(self.d, self.rank_k))
+            self.U = U
+            self.V = numpy.zeros(U.shape)
+            self.w = numpy.zeros((self.d,1))
+            self.b = 0
+        # end if
+
+        if X_is_zscore_normalized == False and X_new is None:
+            X = (X - self.data_mean) / self.data_std
+        if X_is_zscore_normalized == False and X_new is not None:
+            X = X_new
+
+        if self.remaining_train_iter_steps_ < 0: self.remaining_train_iter_steps_ = 0
+        if self.remaining_init_iter_steps_ < 0: self.remaining_init_iter_steps_ = 0
+
+
+
+        if self.is_init_stage_:
+            if n_more_iter is None:
+                the_num_iter = self.remaining_init_iter_steps_
+            else:
+                the_num_iter = n_more_iter
+            ite_count = 0
+            U = self.U
+            p0 = numpy.sum(y)
+            p1 = X.dot(y)
+            cache_1 = self.data_moment3*p1 + self.data_moment4*p0
+            for t in xrange(the_num_iter):
+                ite_count += 1
+                U_new = (ApA_diag0(y,U,X) - cache_1*U)/(2*n)
+                U_new,_ = numpy.linalg.qr(U_new)
+                if numpy.mean(numpy.abs(self.U-U_new)) < self.init_tol:
+                    self.is_init_stage_ = False
+                    self.U = U_new
+                    print 'early stop in initialzation'
+                    break
+                # end if numpy.mean(numpy.abs(self.U-U_new)) < self.init_tol:
+                U = U_new
+            # end for ite_count in xrange(n_more_iter):
+            self.remaining_init_iter_steps_ -= ite_count
+            if not n_more_iter is None: n_more_iter -= ite_count
+
+            # update V
+            V = (ApA_diag0(y,self.U,X) - cache_1*self.U)/(2*n) * self.learning_rate
+            if numpy.linalg.norm(V) > self.lambda_M: V = V / numpy.linalg.norm(V) * self.lambda_M
+            self.V = V
+
+            # update w and b
+            hat_y = A_diag0(self.U,self,V,X)
+            dy = y - hat_y
+            dy = n * dy * sample_weight
+            w = X.dot(dy) / n * self.learning_rate
+            if numpy.linalg.norm(w) > self.lambda_w: w_new = w / numpy.linalg.norm(w) * self.lambda_w
+            if self.learn_w:
+                self.w = w
+            if self.learn_bias_term:
+                self.b = p0
+
+
+
+            if self.remaining_init_iter_steps_ <= 0:
+                print 'stop initialzation'
+                self.is_init_stage_ = False
+            # end if self.remaining_init_iter_steps_ <= 0:
+
+        # end if self.is_init_stage_
+
+        if not self.is_init_stage_:
+            if n_more_iter is None:
+                the_num_iter = self.remaining_train_iter_steps_
+            else:
+                the_num_iter = n_more_iter
+
+            U = self.U
+            V = self.V
+            w = self.w
+            b = self.b
+
+            for t in xrange(the_num_iter):
+                self.remaining_train_iter_steps_ -= 1
+
+                hat_y = A_diag0(U, V, X) + X.T.dot(w) + b
+                dy = y - hat_y
+                dy = n * dy * sample_weight
+                p0 = numpy.sum(dy)
+                p1 = X.dot(dy)
+                cache_1 = self.data_moment3 * p1 + self.data_moment4 * p0
+
+                # update U
+                U_new = (ApA_diag0(y,U,X) - cache_1*U)/(2 * n) * self.learning_rate + \
+                        U.dot(V.T.dot(U)) / 2 + V.dot(U.T.dot(U)) / 2
+                # V_new = U_new
+                U_new, _ = numpy.linalg.qr(U_new)
+
+                # update V
+                V_new = (ApA_diag0(y,U_new,X) - cache_1*U_new) / (2 * n) * self.learning_rate + \
+                        U.dot(V.T.dot(U_new)) / 2 + V.dot(U.T.dot(U_new)) / 2
+                if numpy.linalg.norm(V_new) > self.lambda_M: V_new = V_new / numpy.linalg.norm(V_new) * self.lambda_M
+
+                # update w and b
+                hat_y = A_diag0(U, V, X) + X.T.dot(w) + b
+                dy = y - hat_y
+                dy = n * dy * sample_weight
+                p0 = numpy.sum(dy)
+                p1 = X.dot(dy)
+                cache_1 = self.data_moment3 * p1 + self.data_moment4 * p0
+
+                w_new = X.dot(dy)/n * self.learning_rate + w
+                if numpy.linalg.norm(w_new) > self.lambda_w: w_new = w_new / numpy.linalg.norm(w_new) * self.lambda_w
+
+                b_new = numpy.mean(dy) + b
+
+                if numpy.mean(numpy.abs(U-U_new))<self.tol and numpy.mean(numpy.abs(V-V_new))<self.tol and numpy.mean(numpy.abs(w-w_new))<self.tol and numpy.abs(b-b_new)<self.tol:
+                    U = U_new
+                    V = V_new
+                    if self.learn_w:
+                        w = w_new
+                    if self.learn_bias_term:
+                        b = b_new
+                    print 'early stop'
+                    break
+                # end if ... < tol
+
+                # update old with new variances
+                U = U_new
+                V = V_new
+                if self.learn_w:
+                    w = w_new
+                if self.learn_bias_term:
+                    b = b_new
+            # end for t
+
+            self.U = U
+            self.V = V
+            self.w = w
+            self.b = b
+        # end if not self.is_init_stage_:
+        return self
+    # end def
+
+    def fit_diag_not_zero(self,X,y=None, sample_weight=None, n_more_iter=None, X_is_zscore_normalized = False):
         # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray) -> object
         """
         Train gFM with data X and label y.
@@ -319,58 +538,7 @@ class BatchRegression(BaseEstimator, RegressorMixin):
         :return: The predicted labels
         """
         return self.decision_function(X)
-
-    # def save_model(self,file):
-    #     # type: (str) -> object
-    #     """
-    #     Save gFM model.
-    #     :param file: File-like object or string. Save model to the file.
-    #     :return: self
-    #     """
-    #     numpy.savez_compressed(file, U = self.U, V = self.V, w = self.w,
-    #                            data_mean = self.data_mean, data_std = self.data_std,
-    #                            data_moment3 = self.data_moment3, data_moment4 = self.data_moment4, data_moment5=self.data_moment5,
-    #                            Z = self.Z, G=self.G,
-    #                            feature_selection_bool = self.feature_selection_bool, moment_threshold = self.moment_threshold,
-    #                            max_iter = self.max_iter, max_init_iter = self.max_init_iter, learning_rate=self.learning_rate,
-    #                            lambda_M=self.lambda_M, lambda_w=self.lambda_w, rank_k=self.rank_k,tol=self.tol,init_tol=self.init_tol)
-    #
-    #     return self
-    #
-    # def load_model(self,file):
-    #     """
-    #     Load gFM model from file.
-    #     :param file: File-like object or string. Load model from the file
-    #     :return: self
-    #     """
-    #     the_loaded_file = numpy.load(file)
-    #     self.U = the_loaded_file['U']
-    #     self.V = the_loaded_file['V']
-    #     self.w = the_loaded_file['w']
-    #     self.data_mean = the_loaded_file['data_mean_step1']
-    #     self.data_std = the_loaded_file['data_std']
-    #     self.data_moment3 = the_loaded_file['data_moment3']
-    #     self.data_moment4 = the_loaded_file['data_moment4']
-    #     self.data_moment5 = the_loaded_file['data_moment5']
-    #     self.Z = the_loaded_file['Z']
-    #     self.G = the_loaded_file['G']
-    #     self.feature_selection_bool = the_loaded_file['feature_selection_bool']
-    #     self.moment_threshold = the_loaded_file['moment_threshold']
-    #     self.max_iter = the_loaded_file['max_iter']
-    #     self.max_init_iter = the_loaded_file['max_init_iter']
-    #     self.learning_rate = the_loaded_file['learning_rate']
-    #     self.lambda_M = the_loaded_file['lambda_M']
-    #     self.lambda_w = the_loaded_file['lambda_w']
-    #     self.rank_k = the_loaded_file['rank_k']
-    #     self.tol = the_loaded_file['tol']
-    #     self.init_tol = the_loaded_file['init_tol']
-    #
-    #     return self
-    # # end def
-# end class
-
-
-
+pass # end class
 
 def mathcal_W_(y,X, data_moment3, G,p0,p1,p2,p3):
     # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray) -> numpy.ndarray
@@ -436,3 +604,29 @@ def ApW_(X,p,W):
     z = X.dot(p*X.T.dot(W))
     return z
 # end def
+
+def ApA_diag0(y, U, X):
+    # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray,) -> numpy.ndarray
+    """
+    return A'A(y)*U= (\sum_i x_i y_i x_i' )*U
+    :param y:
+    :param U:
+    :param X:
+    :return:
+    """
+    return (X*y.T).dot(X.T.dot(U))
+pass # end def
+
+def A_diag0(U, V, X):
+    # type: (numpy.ndarray, numpy.ndarray, numpy.ndarray,) -> numpy.ndarray
+    """
+    return A(UV' - diag(UV')) = (X'U * X'V) 1 - X'(((U*V)1)*X)
+    :param U:
+    :param V:
+    :param X:
+    :return:
+    """
+    z = numpy.sum(X.T.dot(U) * X.T.dot(V), axis=1, keepdims=True)
+    z_diag = X.T.dot(numpy.sum(U*V,axis=1,keepdims=True)*X)
+    return z-z_diag
+pass # end def
